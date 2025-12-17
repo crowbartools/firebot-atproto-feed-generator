e83e8fcc4c64dd29f64c7bf332573b224b15b0a1
ebiggz <erikbigler@gmail.com>
2025-12-16 17:59:35 -0700

    api endpoint to add post to feed

--- src/api.ts
+++ src/api.ts
@@ -2,7 +2,7 @@ import Fastify from 'fastify';
 import config from './config.js';
 import { getFirebotPostsFeed } from './lib/feed.js';
 import { AtUri } from '@atproto/syntax';
-import { getAllPosts, postInFeed, removePost } from './lib/db/index.js';
+import { addPost, getAllPosts, postInFeed, removePost } from './lib/db/index.js';
 import { AtpAgent } from '@atproto/api';
 import { getPostAtUri } from './lib/util.js';
 
@@ -92,8 +92,8 @@ server.route({
   },
 });
 
-// remove post from feed
 if (!!config.apiToken?.length) {
+  // remove post from feed
   server.delete('/post', {}, async (req, res) => {
     const { postUrl, token } = req.query as {
       postUrl?: string;
@@ -129,7 +129,7 @@ if (!!config.apiToken?.length) {
     try {
       const response = await agent.resolveHandle({
         handle,
-      });
+      });      
       did = response.data.did;
     } catch (error) {
       res.code(400).send({ error: 'Invalid handle' });
@@ -144,6 +144,65 @@ if (!!config.apiToken?.length) {
     }
 
     await removePost(postUri);
+
+    res.send({
+      success: true,
+    });
+  });
+
+  // add post to feed
+  server.post('/post', {}, async (req, res) => {
+    const { postUrl, token } = req.query as {
+      postUrl?: string;
+      token?: string;
+    };
+
+    if (token !== config.apiToken) {
+      res.code(403).send({ error: 'Invalid token' });
+      return;
+    }
+
+    if (typeof postUrl !== 'string') {
+      res.code(400).send({ error: 'Invalid post URL' });
+      return;
+    }
+
+    // regex to extract the user handle and post rkey from the URL
+    const regex = /https:\/\/bsky\.app\/profile\/([^/]+)\/post\/([^/]+)/;
+    const match = postUrl.match(regex);
+    if (!match) {
+      res.code(400).send({ error: 'Invalid post URL' });
+      return;
+    }
+
+    const handle = match[1] as string;
+    const postRkey = match[2] as string;
+
+    const agent = new AtpAgent({
+      service: 'https://bsky.social',
+    });
+
+    let uri: string | undefined;
+    let cid: string | undefined;
+    try {
+      const post = await agent.getPost({ rkey: postRkey, repo: handle });
+      uri = post.uri;
+      cid = post.cid;
+    } catch (error) {
+      res.code(400).send({ error: 'Invalid handle' });
+      return;
+    }
+
+    if ((await postInFeed(uri))) {
+      res.code(404).send({ error: 'Post already in feed' });
+      return;
+    }
+
+    await addPost({
+      uri,
+      cid,
+      indexedAt: new Date().toISOString(),
+    });
 
     res.send({
       success: true,
